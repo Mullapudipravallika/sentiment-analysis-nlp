bb66c2e79dc2abc270984f62b5d1c4932b467705
Mullapudipravallika <mmullapudipravallika@gmail.com>
2026-03-18 10:57:36 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,35 +1,5 @@
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+import pickle
 
-data = {
-    "text": [
-        "I love this product",
-        "This is a bad experience",
-        "Amazing service",
-        "Very disappointing",
-        "I am happy",
-        "I hate this"
-    ],
-    "label": [1, 0, 1, 0, 1, 0]
-}
-
-df = pd.DataFrame(data)
-
-X_train, X_test, y_train, y_test = train_test_split(
-    df["text"], df["label"], test_size=0.2, random_state=42
-)
-
-vectorizer = TfidfVectorizer()
-X_train_vec = vectorizer.fit_transform(X_train)
-X_test_vec = vectorizer.transform(X_test)
-
-model = LogisticRegression()
-model.fit(X_train_vec, y_train)
-
-predictions = model.predict(X_test_vec)
-accuracy = accuracy_score(y_test, predictions)
-
-print(f"Model Accuracy: {accuracy * 100:.2f}%")
+# after training model
+pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
